7d0fac07f7aecda7aeb46810541fcebb1ff7665f
Niv Shani <niv@Niv-Shani-MacBook-Pro.local>
2019-03-17 23:04:02 +0200

    Anat refactor -- all done

--- ex1/hw1.py
+++ ex1/hw1.py
@@ -6,7 +6,7 @@ np.random.seed(42)
 def preprocess(X, y):
     """
     Perform mean normalization on the features and divide the true labels by
-    the range of the column. 
+    the range of the column.
 
     Input:
     - X: Inputs  (n features over m instances).
@@ -16,14 +16,14 @@ def preprocess(X, y):
     - X: The mean normalized inputs.
     - y: The scaled labels.
     """
-    X = (X - np.average(X, axis=0)) / (np.max(X, axis=0) - np.min(X, axis=0))
-    y = (y - np.average(y, axis=0)) / (np.max(y, axis=0) - np.min(y, axis=0))
+    X = np.subtract(X,np.average(X, axis=0)) / np.subtract(np.max(X, axis=0), np.min(X, axis=0))
+    y = (y - np.average(y)) / (np.max(y) - np.min(y))
     return X, y
 
 def compute_cost(X, y, theta):
     """
     Computes the average squared difference between an observation’s actual and
-    predicted values for linear regression.  
+    predicted values for linear regression.
 
     Input:
     - X: Inputs  (n features over m instances).
@@ -33,15 +33,16 @@ def compute_cost(X, y, theta):
     Returns a single value:
     - J: the cost associated with the current set of parameters (single number).
     """
-    m = len(X)
-    meanSquares = np.sum((np.dot(X, theta) - y) ** 2)
-    J = 1 / (2 * m) * meanSquares
+    m = X.shape[0]
+    errorMat = np.subtract(np.dot(X, theta), y)
+    meanSquares = np.square(errorMat)
+    J = meanSquares.sum(axis=0)
+    J = J / (2*m)
     return J
-
 def gradient_descent(X, y, theta, alpha, num_iters):
     """
     Learn the parameters of the model using gradient descent. Gradient descent
-    is an optimization algorithm used to minimize some (loss) function by 
+    is an optimization algorithm used to minimize some (loss) function by
     iteratively moving in the direction of steepest descent as defined by the
     negative of the gradient. We use gradient descent to update the parameters
     (weights) of our model.
@@ -58,10 +59,10 @@ def gradient_descent(X, y, theta, alpha, num_iters):
     - J_history: the loss value for every iteration.
     """
     J_history = []  # Use a python list to save cost in every iteration
-    m = len(X)
+    m = X.shape[0]
     for i in range(num_iters):
-        sum = np.dot((np.dot(X, theta) - y), X)
-        theta = theta - sum * alpha / m
+        sum = np.dot((np.dot(X, theta) - y), X)/m
+        theta = theta - np.multiply(sum,alpha)
         J_history.append(compute_cost(X, y, theta))
 
     return theta, J_history
@@ -81,9 +82,10 @@ def pinv(X, y):
     ########## DO NOT USE numpy.pinv ##############
     """
 
-    xTranspose = X.T
-    pinvX = np.dot(np.linalg.inv(np.dot(xTranspose, X)), xTranspose)
-    pinv_theta = np.dot(pinvX, y)
+    #xTranspose = X.T
+    pinvX1 = np.linalg.inv(np.matmul(X.T, X))
+    pinvX2 = np.matmul(pinvX1,X.T)
+    pinv_theta = np.matmul(pinvX2, y)
     return pinv_theta
 
 
@@ -106,38 +108,40 @@ def efficient_gradient_descent(X, y, theta, alpha, num_iters):
     """
 
     J_history = []  # Use a python list to save cost in every iteration
-    m = len(X)
+    m = X.shape[0]
     i = 0
     while i < num_iters:
-        sum = np.dot((np.dot(X, theta) - y), X)
-        theta = theta - sum * alpha / m
+        sum1 = np.subtract(np.dot(X, theta) ,y)
+        sum2 = np.dot(sum1,X)/m
+        theta = theta - np.multiply(sum2 ,alpha)
         newCost = compute_cost(X, y, theta)
-        if J_history and J_history[i - 1] - newCost < 1e-8:
-            J_history.append(newCost)
-            break
         J_history.append(newCost)
+        if (i>=1 and J_history[i - 1] - newCost < 1e-8):
+            break
+        
         i += 1
     return theta, J_history
 
 
 def find_best_alpha(X, y, iterations):
     """
-    Iterate over provided values of alpha and maintain a python dictionary 
+    Iterate over provided values of alpha and maintain a python dictionary
     with alpha as the key and the final loss as the value.
 
     Input:
     - X: a dataframe that contains all relevant features.
 
     Returns:
-    - alpha_dict: A python dictionary that hold the loss value after training 
+    - alpha_dict: A python dictionary that hold the loss value after training
     for every value of alpha.
     """
 
     alphas = [0.00001, 0.00003, 0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 2, 3]
-    startingTheta = [-1, 2]
     alpha_dict = {}
     for alpha in alphas:
-        J_history = efficient_gradient_descent(X, y, startingTheta, alpha, iterations)[1]
+        np.random.seed(42)
+        theta = np.random.random(size=2)
+        J_history = efficient_gradient_descent(X, y, theta, alpha, iterations)[1]
         lastCost = J_history[-1]
         alpha_dict[alpha] = lastCost
 
@@ -163,8 +167,8 @@ def generate_triplets(X):
 
 def find_best_triplet(df, triplets, alpha, num_iter):
     """
-    Iterate over all possible triplets and find the triplet that best 
-    minimizes the cost function. For better performance, you should use the 
+    Iterate over all possible triplets and find the triplet that best
+    minimizes the cost function. For better performance, you should use the
     efficient implementation of gradient descent. You should first preprocess
     the data and obtain a array containing the columns corresponding to the
     triplet. Don't forget the bias trick.
